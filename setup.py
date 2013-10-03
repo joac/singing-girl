c4d69b5624a6d04241ea162a22fa281d5eaf8e55
Joaquín Sorianello <joac@joac.com.ar>
2013-10-03 13:41:22 -0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+#! -*- coding: utf8 -*-
+from distutils.core import setup
+
+setup(
+    name='singing-girl',
+    version='0.0.1',
+    description=u'Library to convert number literals, to spanish equivalent text strings',
+    long_description=open('README.md').read(),
+    author=u'Joaquín Sorianello',
+    url='https://github.com/joac/singing-girl',
+    packages=['singing_girl'],
+    license='LICENSE.md',
+    classifiers = [
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: Spanish',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Development Status :: 5 - Production/Stable',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Localization',
+        'Topic :: Utilities'
+    ],
+)
