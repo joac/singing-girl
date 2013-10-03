1a9c3ce831ae1763745ceffd752f028a963934b7
Joaquín Sorianello <joac@joac.com.ar>
2013-10-03 12:19:25 -0300

    Improve to support python3

--- singing_girl/__init__.py
+++ singing_girl/__init__.py
@@ -1,10 +1,12 @@
 #! -*- coding: utf8 -*-
+from __future__ import print_function
 from singer import Singer
 
 
 if __name__ == '__main__':
+
     t = Singer()
-    print t.sing(1000000)
+    print(t.sing(1000000))
 
 
 
