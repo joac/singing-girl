cdfe4b69e5c39da9fdf9270ef7ddf93b293f9029
Joaquín Sorianello <joac@joac.com.ar>
2015-04-29 20:39:54 -0300

    Remove unneed code

--- singing_girl/__init__.py
+++ singing_girl/__init__.py
@@ -1,5 +1,4 @@
 #! -*- coding: utf8 -*-
-from __future__ import print_function
 from .singer import Singer
 
 _singer_instance = Singer()
@@ -7,13 +6,3 @@ _singer_instance = Singer()
 def sing(number):
     """Utility function for easier usage"""
     return _singer_instance.sing(number)
-
-
-if __name__ == '__main__':
-
-    t = Singer()
-    print(t.sing(1000000))
-
-
-
-
